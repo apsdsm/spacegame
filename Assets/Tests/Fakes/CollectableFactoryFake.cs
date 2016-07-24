34fe00539427b39ea403c1428186f18a5c8b41c2
Nick del Pozo <nickdelpozo@gmail.com>
2016-07-24 18:52:58 +0900

    collectale factory fake

---
+++ Assets/Tests/Fakes/CollectableFactoryFake.cs
@@ -0,0 +1,10 @@
+﻿using SpaceGame.Interfaces;
+using TestHelpers;
+
+namespace SpaceGame.Tests.Fakes
+{
+    class CollectableFactoryFake : UFake, ICollectableFactory
+    {
+        public ICollectable CreateCoin () { return evaluateMethod<ICollectable>("CreateCoin"); }
+    }
+}
